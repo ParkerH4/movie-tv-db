274c1a92e63bafae0a7c4c148eaf4ba1437cc447
Parker Hewitt <parkerhewitt95@gmail.com>
2023-05-20 18:19:06 -0600

    Fetched movie data from API and got it to display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+import MovieCard from "./components/MovieCard.js";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const BASE_URL = "https://api.themoviedb.org/3";
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const {
+      data: { results },
+    } = await axios.get(`${BASE_URL}/discover/movie`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+
+    setMovies(results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  });
+
+  const renderMovies = () => movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+
+  return (
+    <div className="App">
+      <h1>MovieDB App</h1>
+      <div className="container">{renderMovies()}</div>
+    </div>
+  );
+};
 
 export default App;
